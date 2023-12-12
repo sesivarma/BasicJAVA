6db97af8909cca0ec82065c5cb5a2caa0e8eb2a4
sesivarma <sasevarma@gmail.com>
2023-12-12 16:53:34 +0530

    first commit

---
+++ Dog.java
@@ -0,0 +1,24 @@
+public class Dog {
+
+    int legs;
+    int tail;
+    String color;
+    int mouth;
+
+
+    
+    public void run()
+    {
+
+        System.out.println(" run with "+legs+"legs");
+
+    }
+
+    public void eat()
+    {
+        System.out.println("");
+        System.out.println(false
+        );
+    }
+    
+}
